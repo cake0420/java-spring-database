80f029ef7be6c54c84d785695635f74bd675572b
soowan <nick6767676767@gmail.com>
2025-04-07 14:58:29 +0900

    feat: 트랜잭션 매니저 추가

--- src/main/java/com/cake7/database/config/DatabaseConfig.java
+++ src/main/java/com/cake7/database/config/DatabaseConfig.java
@@ -3,7 +3,9 @@ package com.cake7.database.config;
 import com.cake7.database.util.EnvLoader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.transaction.PlatformTransactionManager;
 
 import javax.sql.DataSource;
 
@@ -18,5 +20,10 @@ public class DatabaseConfig {
         dataSource.setUsername(EnvLoader.get("DB_USERNAME"));
         dataSource.setPassword(EnvLoader.get("DB_PASSWORD"));
         return dataSource;
+    }
+
+    @Bean
+    public PlatformTransactionManager transactionManager(DataSource dataSource) {
+        return new DataSourceTransactionManager(dataSource);
     }
 }
---
+++ src/test/java/SignUpServiceTest.java
@@ -0,0 +1,102 @@
+import com.cake7.database.config.AppConfig;
+import com.cake7.database.config.DatabaseConfig;
+import com.cake7.database.domain.Users;
+import com.cake7.database.model.repository.UserRepository;
+import com.cake7.database.util.Encrypt;
+import com.cake7.database.util.UuidToBinary;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringJUnitConfig(classes = {AppConfig.class, DatabaseConfig.class})
+@WebAppConfiguration
+@Transactional
+@Rollback
+@TestInstance(TestInstance.Lifecycle.PER_CLASS) // JUnit이 생성자 주입 허용
+public class SignUpTest {
+
+    private final UserRepository userRepository;
+    private final UuidToBinary uuidToBinary;
+    private final Encrypt encrypt;
+    private Users testUser;
+
+    @Autowired
+    public SignUpTest(UserRepository userRepository, UuidToBinary uuidToBinary, Encrypt encrypt) {
+        this.userRepository = userRepository;
+        this.uuidToBinary = uuidToBinary;
+        this.encrypt = encrypt;
+    }
+
+    @BeforeEach
+    void setUp() {
+        UUID uuid = UUID.randomUUID();
+        byte[] binaryUuid = uuidToBinary.uuidToBytes(uuid);
+        String password = "q1w@e3r$";
+        String salt = encrypt.generateSalt();
+        String newPassword = encrypt.getEncrypt(password, salt);
+
+        testUser = new Users(
+                binaryUuid,
+                "테스트 유저",
+               "test@example.com",
+                newPassword,
+                salt
+        );
+    }
+
+    @Test
+    void existByEmail() throws Exception {
+        String email = "test@example.com";
+
+        boolean exist = userRepository.existByEmail(email);
+        assertFalse(exist, "존재하지 않는 이메일은 false를 반환해야 합니다.");
+    }
+
+    @Test
+    void save_and_existByEmail() throws Exception {
+        String email = "test2@example.com";
+
+        UUID uuid = UUID.randomUUID();
+        byte[] binaryUuid = uuidToBinary.uuidToBytes(uuid);
+        String password = "q1w@e3r$";
+        String salt = encrypt.generateSalt();
+        String newPassword = encrypt.getEncrypt(password, salt);
+
+        testUser = new Users(
+                binaryUuid,
+                "테스트 유저",
+                email,
+                newPassword,
+                salt
+        );
+
+        userRepository.save(testUser);
+        boolean exist = userRepository.existByEmail(email);
+
+        assertTrue(exist, "저장된 이메일은 existByEmail에서 true를 반환해야 합니다.");
+
+    }
+
+    @Test
+    void entityToMap_valid() throws Exception {
+        // when
+        var map = userRepository.entityToMap(testUser);
+
+        // then
+        assertEquals(testUser.getId(), map.get("id"));
+        assertEquals(testUser.getName(), map.get("name"));
+        assertEquals(testUser.getEmail(), map.get("email"));
+        assertEquals(testUser.getPassword(), map.get("password"));
+        assertEquals(testUser.getSalt(), map.get("salt"));
+    }
+
+}
